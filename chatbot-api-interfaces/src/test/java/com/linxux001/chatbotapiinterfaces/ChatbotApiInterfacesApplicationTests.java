20ff4d2d49c1bc6a6560e82b06e851c178ccbb20
linxuxu <git.tohi.cn>
2025-11-26 22:30:45 +0800

    初始化工程

---
+++ chatbot-api-interfaces/src/test/java/com/linxux001/chatbotapiinterfaces/ChatbotApiInterfacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linxux001.chatbotapiinterfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotApiInterfacesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
